027148b102c3d677510624ed859250338f025bb9
허예은 <20211415@sungshin.ac.kr>
2023-03-05 23:12:41 +0900

    5.Order domain

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -13,6 +15,7 @@ import static javax.persistence.FetchType.LAZY;
 @Entity
 @Table(name="orders") //적어주지 않으면 관례상 order가 되는데, 쿼리와 같아 오류남
 @Getter @Setter
+@NoArgsConstructor(access=AccessLevel.PROTECTED) //new Order()등 형태 생성 막음
 public class Order {
 
     @Id @GeneratedValue
@@ -49,5 +52,41 @@ public class Order {
     public void setDelivery(Delivery delivery){
         this.delivery=delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member,Delivery delivery,OrderItem... orderItems){
+        Order order=new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem:orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    //주문 취소
+    public void cancel(){
+        if (delivery.getStatus()==DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem:orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    //전체 주문 가격 조회
+    public int getTotalPrice(){
+        int totalPrice=0;
+        for(OrderItem orderItem:orderItems){
+            totalPrice+=orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,13 +12,14 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access=AccessLevel.PROTECTED) //new OrderItem()등 형태 생성 막음
 public class OrderItem {
 
     @Id @GeneratedValue
     @Column(name="order_item_id")
     private Long id;
 
-    @ManyToOne(fetch=LAZY) //*->1(단방향)이므로 반대쪽에 mappedBy없음
+    @ManyToOne(fetch=LAZY) //*->1(단방향)이므로 반대쪽에 mappedBy 없음
     @JoinColumn(name="item_id")
     private Item item;
 
@@ -26,4 +29,26 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item,int orderPrice,int count){
+        OrderItem orderItem=new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    //주문상품 전체 가격 조회
+    public int getTotalPrice(){
+        return getOrderPrice()*getCount();
+    }
 }
